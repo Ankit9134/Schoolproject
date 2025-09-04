af0123dcbca8b36e8f47fa68eb3ed3dbba3987ef
Ankit <ap9377016@gmail.com>
2025-09-04 23:39:37 +0530

    Update route.js

--- app/api/schools/route.js
+++ app/api/schools/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { getPool } from '@/lib/db';
+import { getPool } from '../..lib/db';
 import { IncomingForm } from 'formidable';
 import fs from 'fs';
 import path from 'path';
